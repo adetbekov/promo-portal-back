fe21df14eecf5078037e360e66562ea8fd40b8c4
adetbekov <dosya@inbox.ru>
2019-07-09 22:27:35 +0600

    cors fix

--- promobackend/promobackend/settings.py
+++ promobackend/promobackend/settings.py
@@ -56,11 +56,14 @@ INSTALLED_APPS = [
 
     "accounts",
     'cities_light',
+    'corsheaders'
 ]
 
 SITE_ID = 1
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.BrokenLinkEmailsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
